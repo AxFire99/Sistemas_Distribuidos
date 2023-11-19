547a910d5d1537502dcb8b0ff7d2e8d559a10160
Andrés Millán <45878709+Oni67@users.noreply.github.com>
2023-11-19 12:18:10 +0000

    Add files via upload

--- notifications/forms.py
+++ notifications/forms.py
@@ -3,4 +3,4 @@ from django import forms
 from django.contrib.auth.models import User  # Import the User model
 
 class UserSelectForm(forms.Form):
-    user = forms.ModelChoiceField(queryset=User.objects.all(), empty_label=None)
+    user = forms.ModelChoiceField(queryset=User.objects.all(), empty_label="Choose your option", to_field_name='username')
