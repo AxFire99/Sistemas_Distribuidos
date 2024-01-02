8df347b64d1f7afec4dad285a13f3dd7435cfd8a
Vitor Freitas <96492874+MrMCchicken@users.noreply.github.com>
2024-01-02 15:51:53 +0000

    Update settings.py

--- djangokubernetesproject/settings.py
+++ djangokubernetesproject/settings.py
@@ -81,11 +81,17 @@ WSGI_APPLICATION = "djangokubernetesproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django_prometheus.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_prometheus.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_cache',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
