3bd76a5b64caa19f25f9ab0b70b94ad8711c93a4
Vitor Freitas <96492874+MrMCchicken@users.noreply.github.com>
2024-01-02 15:43:44 +0000

    Update settings.py

--- djangokubernetesproject/settings.py
+++ djangokubernetesproject/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django_prometheus',
     "notifications",
     'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = "djangokubernetesproject.urls"
