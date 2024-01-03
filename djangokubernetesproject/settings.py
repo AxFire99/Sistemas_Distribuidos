a8eea3e11f37287c216cbe34e29daa30e5fff833
Vitor Freitas <96492874+MrMCchicken@users.noreply.github.com>
2024-01-03 12:40:44 +0000

    Update settings.py

--- djangokubernetesproject/settings.py
+++ djangokubernetesproject/settings.py
@@ -136,5 +136,3 @@ EMAIL_HOST_PASSWORD = 'fkzr noxq wyai odso' #nosec
 
 
 PROMETHEUS_LATENCY_BUCKETS = (0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0, 25.0, 50.0, 75.0, float("inf"),)
-
-PROMETHEUS_METRIC_NAMESPACE = "metric"
